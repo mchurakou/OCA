2c19d03b982fd1c5f023190a3316042bd4500b47
mchurakou <1badbug1@gmail.com>
2016-10-11 23:10:05 +0300

    Initial commit

---
+++ src/oca/tip/Run.java
@@ -0,0 +1,14 @@
+package oca.tip;
+
+/**
+ * Created by mikalai on 11.10.2016.
+ */
+public class Run {
+    public static void main(String[] args) {
+        float value1 = 102;
+        float value2 = (int)102.0;
+        float value3 = 1f * 0.0;
+        float value4 = 1f * (short)0.0;
+        float value5 = 1f * (boolean)0;
+    }
+}
