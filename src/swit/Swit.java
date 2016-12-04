cf96bcbbebdccf151d5c291a3e7dab5dee6e155b
Mikalai Churakou <1badbug1@gmail.com>
2016-12-04 22:41:59 +0300

    - switch

---
+++ src/swit/Swit.java
@@ -0,0 +1,26 @@
+package swit;
+
+/**
+ * Created by mikalai on 12/4/2016.
+ */
+public class Swit {
+
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        final int  z = 3;
+
+        switch (5 % 2){
+            default:
+                System.out.println(3);
+            case: 5 / 1
+                System.out.println(5);
+            case: z
+                System.out.println(z);
+            case: 'a' :
+                System.out.println(44);
+            case: 3
+                System.out.println(33);
+        }
+    }
+}
